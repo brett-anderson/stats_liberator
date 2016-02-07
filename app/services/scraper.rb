99535aa97d31a755aa564a25f6bcad57163f09de
brett-anderson <brettanderson16@gmail.com>
2016-02-06 16:59:21 -0800

    fix for finding missing players

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -13,25 +13,34 @@ class Scraper
 
   def call
     Player.where(name: nil).destroy_all
-    id = Player.last ? Player.last.id : 1
 
-    iterations = 0
+    id = 1
+    players_processed = 0
 
-    while iterations < 25
-      doc = Nokogiri::HTML(open("http://sports.yahoo.com/nhl/players/#{id}/"))
-      player = Player.create(html: doc.to_s)
+    while players_processed < 40
+      player = player.where(id: id)
 
-      player.name = doc.at_css('.player-info h1') ? doc.at_css('.player-info h1').attributes['data-name'].value : nil
-      player.yahoo_id = id
-      Rails.logger.info "id: #{id}, player: #{player.name}"
-      if player.name && player.save
-        Rails.logger.info "#{player.name} saved."
-      else
-        Rails.logger.info "failed to save."
+      unless player.count > 0
+        player = find_player(id)
+        Rails.logger.info "id: #{id}, player: #{player.name}"
+        if player.name && player.save
+          Rails.logger.info "#{player.name} saved."
+        else
+          Rails.logger.info "failed to save with id: #{id}."
+        end
+        sleep 15.seconds
+        players_processed = players_processed + 1
       end
-      iterations = iterations + 1
-      id = id + 1
-      sleep 15.seconds
     end
   end
+
+  private
+
+  def find_player(id)
+    doc = Nokogiri::HTML(open("http://sports.yahoo.com/nhl/players/#{id}/"))
+    player = Player.create(html: doc.to_s)
+    player.name = doc.at_css('.player-info h1') ? doc.at_css('.player-info h1').attributes['data-name'].value : nil
+    player.yahoo_id = id
+  end
+
 end
