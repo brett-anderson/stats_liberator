c1c96727aebfc91378e6f1d5b10e34445faef07b
brett-anderson <brettanderson16@gmail.com>
2016-02-06 22:00:55 -0800

    create table

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,8 @@
 class PlayersController < ApplicationController
   def index
   #  @players = Player.all.limit(20)
-   @names = Player.all.pluck(:name).uniq
+   @names = Player.all.pluck(:name, :yahoo_id).uniq
+
   end
   def show
    @player = Player.find(params[:id])
