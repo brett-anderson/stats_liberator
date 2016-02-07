1c984f46da72bae69f6bed596204e9bb01594a50
brett-anderson <brettanderson16@gmail.com>
2016-02-06 21:50:57 -0800

    Change index

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,9 @@
 class PlayersController < ApplicationController
   def index
-   @players = Player.all.limit(20)
+  #  @players = Player.all.limit(20)
+   @names = Player.all.pluck(:name).uniq
   end
   def show
    @player = Player.find(params[:id])
-   @names = Player.all.pluck(:name).uniq
   end
 end
