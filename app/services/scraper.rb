821ca8a8b6b1a88ac0e01e4d451f0ef5a4fb05bd
brett-anderson <brettanderson16@gmail.com>
2016-02-06 16:38:17 -0800

    Add better error handling

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -12,15 +12,16 @@ class Scraper
   end
 
   def call
-
-    id = Player.last ? Player.last.yahoo_id : 1
+    Player.where(name: nil).destroy_all
+    id = Player.last ? Player.last.id : 1
 
     iterations = 0
 
     while iterations < 25
       doc = Nokogiri::HTML(open("http://sports.yahoo.com/nhl/players/#{id}/"))
       player = Player.create(html: doc.to_s)
-      player.name = doc.at_css('.player-info h1').attributes['data-name'].value
+
+      player.name = doc.at_css('.player-info h1').attributes ? doc.at_css('.player-info h1').attributes['data-name'].value
       player.yahoo_id = id
       if player.save
         Rails.logger.info "#{Player.name} saved."
